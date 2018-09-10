a52a13cb062f249760bcd865c95d308631bf58c9
rrriiissshhh <rishrocks17@gmail.com>
2018-09-11 00:36:18 +0530

    Okish retargetting, copying to skill files, almost correct model for our nao robot

---
+++ Blender_files/retarget.py
@@ -0,0 +1,59 @@
+from bvh import Bvh
+
+with open('Retargetting/walk_smooth.bvh') as f:
+	mocap = Bvh(f.read())
+
+with open('model/constraints_updated.txt') as f:
+	content = f.readlines()
+
+content = [x.strip() for x in content] 
+content = [x.split() for x in content] 
+channel_mapping = ['Xrotation', 'Yrotation', 'Zrotation']
+
+mapping = {}
+for joint in content:
+	mapping[joint[0]] = []
+	for i in range(1,4):
+		if (joint[i] != '0'):
+			mapping[joint[0]].append((channel_mapping[i-1], joint[i]))
+
+start_skill_str = "STARTSKILL SKILL_TEST\n"
+start_frame_str = "STARTSTATE\n"
+end_frame_str   = "ENDSTATE\n"
+end_skill_str = "ENDSKILL\n"
+
+theta = 3
+prev_frame = 0
+k = 0.5
+
+with open('model/test.skl', 'w') as f:
+	f.write(start_skill_str+ "\n")	
+	
+	
+	for frame in range(0, min(mocap.nframes, 120), 3):
+		if (frame == 0):
+			frame_data_str = "settar "
+		else	:
+			frame_data_str = "inctar "
+		wait_frame_str  = "wait " + str(round(k * mocap.frame_time * (frame - prev_frame),1)) + " end\n"
+		write = False
+		for joint in mocap.get_joints_names():
+			for channel in mapping[joint]:
+				this_frame_val = mocap.frame_joint_channel(frame, joint, channel[0])
+				if frame == 0:
+					prev_frame_val = 0	
+				else:
+					prev_frame_val = mocap.frame_joint_channel(prev_frame, joint, channel[0])
+
+				print(frame, prev_frame, channel, this_frame_val, prev_frame_val)
+				if "A" not in channel[1] and abs(this_frame_val - prev_frame_val) > theta: 
+					write = True
+					frame_data_str += " " + str(channel[1]) + " " + str(round(this_frame_val - prev_frame_val, 1))
+		if (write):
+			prev_frame = frame		
+			f.write(start_frame_str)
+			f.write(frame_data_str + " end \n")
+			f.write(wait_frame_str)
+			f.write(end_frame_str + "\n")
+
+	f.write(end_skill_str)

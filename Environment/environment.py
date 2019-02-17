Normalization corrected, layer structure changed

Environment/environment.py
@@ -15,10 +15,10 @@ class Environment(object):
     # Global Constants
     TEAM = "UTAustinVilla_Base"
     U_NUM = 1
-    SIMULATION_TIME = 4
+    SIMULATION_TIME = 7.8
 
     #Connection to Server and Motion Clip
-    MOTION_CLIP = CWD + "/imitation/debug/hands_opposite_ow.bvh"
+    MOTION_CLIP = CWD + "/imitation/debug/stand.bvh"
     CONSTRAINTS = CWD + "/imitation/constraints.txt"
     
     # Server and Monitor
@@ -55,8 +55,8 @@ class Environment(object):
 
     DIM = len(STATE_KEYS)
     DEFAULT_ACTION = np.zeros(len(STATE_KEYS));
-    DEFAULT_STATE_MIN = np.array([-120,-120, -5,-5, -10,-10,-10, -160,-15,-15, -0.01,-2,-2, 80, 0])
-    DEFAULT_STATE_RANGE = np.array([150, 150, 5,5, 5,5,5, 150,15,15, 0.02,1,1, 2, 4])
+    DEFAULT_STATE_MIN = np.concatenate([np.ones(3*len(STATE_KEYS)) * -120, np.array([-10,-10,-10, -160,-15,-15, -0.01,-2,-2, 80, 0])])
+    DEFAULT_STATE_RANGE = np.concatenate([np.ones(3*len(STATE_KEYS)) * 100, np.array([5,5,5, 150,150,150, 0.02,1,1, 100, 4])])
 
     DEFAULT_REWARD_MIN = -2
     DEFAULT_REWARD_RANGE = 2
@@ -73,7 +73,7 @@ class Environment(object):
         self.agent_port = agent_port
         self.monitor_port = monitor_port
 
-        self.state_dim = self.DIM*2  + 11
+        self.state_dim = self.DIM*3  + 11
         self.action_dim = self.DIM
         
         self.agent = BaseAgent(host=host, port=agent_port, teamname=self.TEAM, player_number=self.U_NUM)
@@ -92,7 +92,8 @@ class Environment(object):
         self.min_a = 100 * np.ones(self.state_dim)
         self.max_r = 0
         self.min_r = 100
-        
+        self.prev_state = {}
+
     def map_action(self, action, sp=False):
         tmp = {}
         for i, s in enumerate(self.STATE_KEYS):
@@ -119,9 +120,10 @@ class Environment(object):
                 frame = [str(a) for a in frame]
                 f.write(" ".join(frame) + "\n")
 
-    def demap_state(self, state, acc, gyr, pos, orr, velocities, time):
+    def demap_state(self, state, acc, gyr, pos, orr, velocities, target, time):
         tmp = [state[s]for s in self.STATE_KEYS]
         tmp = tmp + list(velocities)
+        tmp = tmp + list(target)
         tmp = tmp + list(acc)
         tmp = tmp + list(gyr)
         tmp = tmp + list(pos)
@@ -134,42 +136,51 @@ class Environment(object):
         self.min_a = np.minimum(self.min_a, np.array(tmp))        
         return tmp         
 
+    def get_velocity(self, state):
+        if self.prev_state:
+            tmp = []
+            for s in self.STATE_KEYS:
+                tmp.append(state[s] - self.prev_state[s])
+            self.prev_state = state
+            return np.array(tmp)
+        else:
+            self.prev_state = state
+            return self.DEFAULT_ACTION 
+
     def step(self, action, sp=False):
         try:
             state, acc, gyr, pos, orr, time, is_fallen = self.agent.step(self.map_action(action, sp))
             self.update_motion(state)
-
-            s = self.demap_state(state, acc, gyr, pos, orr, action, time)
-            r = self.generate_reward(state, acc, gyr, action, time, is_fallen)  
-            return s, r, self.time_up(time), None        
+            vel = self.get_velocity(state)
+            target, r = self.generate_reward(state, time, is_fallen)  
+            s = self.demap_state(state, acc, gyr, pos, orr, vel, target, time)
+            return s, r, is_fallen or self.time_up(time), None        
         
         except (BrokenPipeError, ConnectionResetError, ConnectionRefusedError, socket.timeout, struct.error):
             return None, 0, True, None
     
-    def generate_reward(self, state, acc, gyr, action, time, is_fallen):
-        sim = self.motion_clip.similarity(time - self.time, state, self.STATE_KEYS)
-        if sim is not None:
-            reward = 10 + sim
-        else:
-            reward = 0
-
+    def generate_reward(self, state, time, is_fallen):
+        target, sim = self.motion_clip.similarity(time - self.time, state, self.STATE_KEYS)
         # act_rew = 0
         # for a in action:
         #     if math.fabs(a) > 0.5:
         #         act_rew += (math.fabs(a) - 0.5) ** 2
         # act_rew = 500 * math.sqrt(act_rew)
         # reward -= act_rew 
+        reward = sim * 0.0001
         # if is_fallen:
-        #     print('(generate_reward) fallen ', acc)
-        #     reward -= 100000
+        #     print('(generate_reward) fallen ', reward)
+        #     reward -= 5000 * np.exp(-(time - self.time))
         # elif self.time_up(time):
-        #     reward += 10000
-        # print(sim, act_rew, reward)
-        reward = reward * 0.01
-        reward = (reward - self.DEFAULT_REWARD_MIN)/self.DEFAULT_REWARD_RANGE
+        #     reward += 5000
+
+        # reward = (reward - self.DEFAULT_REWARD_MIN)/self.DEFAULT_REWARD_RANGE
         self.min_r = min(reward, self.min_r)
         self.max_r = max(reward, self.max_r)
-        return reward
+        tmp = []
+        for s in self.STATE_KEYS:
+            tmp.append(target[s])
+        return np.array(tmp), reward
 
     def reset(self):
         self.count_reset += 1

a3c/A3C.py
@@ -19,7 +19,7 @@ NUM_THREADS = 3
 os.environ["OMP_NUM_THREADS"] = str(NUM_THREADS)
 
 GAMMA = 1
-MAX_EP = 2000 # Max episodes
+MAX_EP = 7000 # Max episodes
 MAX_EP_STEP = 500 # Max episode step
 
 env = Environment()
@@ -27,8 +27,8 @@ N_S = env.state_dim
 N_A = env.action_dim
 
 Z = 200 # number of hidden nodes in each layer
-SPAN = 0.5 # in radians per sec
-DELTA = 0.001 # minimum sigma
+# SPAN = 0.5 # in radians per sec
+# DELTA = 0.0001 # minimum sigma
 
 # modelName = 'upper_body+2legjoint.pt'
 # modelName = 'hand_opposite_net.pt'
@@ -37,7 +37,7 @@ DELTA = 0.001 # minimum sigma
 modelName = 'int_net.pt'
 loadModel = False
 testModel = False
-learning_rate = 0.0005
+learning_rate = 0.00001
 
 is_gpu_available = torch.cuda.is_available()
 class Net(nn.Module):
@@ -46,14 +46,11 @@ class Net(nn.Module):
         self.s_dim = s_dim
         self.a_dim = a_dim
         
-        self.com = nn.Linear(s_dim, Z)
+        self.com1 = nn.Linear(s_dim, Z)
+        self.com2 = nn.Linear(Z, Z)
+        self.com3 = nn.Linear(Z, Z)
         
-        self.a1 = nn.Linear(Z, Z)
-        self.a2 = nn.Linear(Z, Z)
         self.mu = nn.Linear(Z, a_dim)
-        
-        self.b1 = nn.Linear(Z, Z)
-        self.b2 = nn.Linear(Z, Z)
         self.sigma = nn.Linear(Z, a_dim)
         
 
@@ -62,20 +59,16 @@ class Net(nn.Module):
         self.c3 = nn.Linear(Z, Z)
         self.v = nn.Linear(Z, 1)
         
-        set_init([self.com, self.a1, self.a2, self.b1, self.b2, self.mu, self.sigma, self.c1, self.c2, self.c3, self.v])
+        set_init([self.com1, self.com2, self.com3, self.mu, self.sigma, self.c1, self.c2, self.c3, self.v])
         self.distribution = torch.distributions.Normal
 
     def forward(self, x):
-        com = F.relu6(self.com(x))
-        a1 = F.relu6(self.a1(com))
-        a2 = F.relu6(self.a2(a1))
-        mu = SPAN * torch.tanh(self.mu(a2))
-        # print("MU", self.mu(a1))
-
-        b1 = F.relu6(self.b1(com))
-        b2 = F.relu6(self.b2(b1))
-        sigma = F.softplus(self.sigma(b2)) + DELTA # TODO Is delta necessary 
-        # print("SIGMA", self.sigma(b1))
+        com1 = F.relu6(self.com1(x))
+        com2 = F.relu6(self.com2(com1))
+        com3 = F.relu6(self.com3(com2))
+        
+        mu = self.mu(com3)
+        sigma = F.softplus(self.sigma(com3)) 
 
         c1 = F.relu6(self.c1(x))
         c2 = F.relu6(self.c2(c1))
@@ -176,12 +169,12 @@ class Worker(mp.Process):
                     # Update Global And Local Neural Nets After This Episode
                     push_and_pull(self.opt, self.lnet, self.gnet, done, s_, buffer_s, buffer_a, buffer_r, GAMMA, is_gpu_available)
                     
-                    print("(MAX)",self.env.max_a)
-                    print("(MIN)",self.env.min_a)
-                    print("(DIFF)",self.env.max_a - self.env.min_a)
-                    print("(max_R)",self.env.max_r)
-                    print("(min_R)",self.env.min_r)
-                    print("(diff)",self.env.max_r - self.env.min_r)
+                    # print("(MAX)",self.env.max_a)
+                    # print("(MIN)",self.env.min_a)
+                    # print("(DIFF)",self.env.max_a - self.env.min_a)
+                    # print("(max_R)",self.env.max_r)
+                    # print("(min_R)",self.env.min_r)
+                    # print("(diff)",self.env.max_r - self.env.min_r)
                     print("(Total_Reward)",ep_r)                    
                     
                     # Record the cumulative reward and update average